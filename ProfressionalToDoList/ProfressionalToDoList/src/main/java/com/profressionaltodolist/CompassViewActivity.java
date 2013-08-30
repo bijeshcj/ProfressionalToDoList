4fb7bf22acdc6c891b7f2788d6780207a3dc0017
NPCompete <npcompete@npcompete-desktop.(none)>
2013-08-30 17:01:24 +0530

    CompassView and activity added

---
+++ ProfressionalToDoList/ProfressionalToDoList/src/main/java/com/profressionaltodolist/CompassViewActivity.java
@@ -0,0 +1,15 @@
+package com.profressionaltodolist;
+
+import android.os.Bundle;
+
+/**
+ * Created by npcompete on 30/8/13.
+ */
+public class CompassViewActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.compassview);
+    }
+}
